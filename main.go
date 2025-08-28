eeaa1a17668f754718077fa26447fe222f8f1f79
Ekke Nekkepenn <denizumiekren@gmail.com>
2025-08-28 17:06:28 +0200

    added 'markAsDone' works internally

--- main.go
+++ main.go
@@ -22,10 +22,12 @@ type Task struct {
 
 func (t *Task) print() {
 	var char_status rune
-	if t.status {
-		char_status = 'X'
+	if !t.status {
+		//Unicode Character “✗” (U+2717)
+		char_status = '✗'
 	} else {
-		char_status = 'O'
+		//Unicode Character “✓” (U+2713)
+		char_status = '✓'
 	}
 
 	fmt.Printf("%s\t%c\n", t.content, char_status)
@@ -33,19 +35,36 @@ func (t *Task) print() {
 
 //-------------------------------------------------------------------------------------
 
-// stores Tasks
-type TaskContainer struct {
-	// slice for Tasks
-	slc []Task
+type List struct {
+	s []Task
 }
 
-func (tc *TaskContainer) addTask(t Task) {
-	if len(tc.slc) >= cap(tc.slc) {
-		fmt.Printf("Cannot store more task. Max amount: %d", capacity)
+func (l *List) appendTask(t Task) {
+	if len(l.s) >= cap(l.s) {
+		fmt.Printf("Max amount of Tasks reached (%d). Please remove some Task to add more", capacity)
 	}
-	// append can resize the underlying array so we check before
-	tc.slc = append(tc.slc, t)
+	l.s = append(l.s, t)
 }
+
+func (l *List) markAsDone(n int) {
+	// before calling this func check if 0 <= n && n < len(l.s)
+	if 0 < n && len(l.s) < n {
+		fmt.Printf("len of l.s %d\n", len(l.s))
+		fmt.Printf("Given index %d outside range of todolist\n", n)
+		return
+	}
+	l.s[n].status = true
+	fmt.Println("status changed")
+}
+
+func (l *List) printTasks() {
+	for idx, task := range l.s {
+		fmt.Printf("%d.  ", idx+1)
+		task.print()
+	}
+}
+
+//-------------------------------------------------------------------------------------
 
 func check(e error) {
 	if e != nil {
@@ -53,7 +72,7 @@ func check(e error) {
 	}
 }
 
-func loadData(tc *TaskContainer) {
+func loadData(l *List) {
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 	check(err)
 	defer file.Close()
@@ -77,29 +96,75 @@ func loadData(tc *TaskContainer) {
 
 		var status bool
 		// squiggly lines?
-		if parts[2] == "x" {
+		if strings.ToLower(parts[2]) == "x" {
 			status = false
-		} else if parts[2] == "o" {
+		} else if strings.ToLower(parts[2]) == "o" {
 			status = true
 		} else {
 			fmt.Println("Status data in file is wrong")
 			panic(status)
 		}
 
-		t := Task{
+		l.appendTask(Task{
 			content: parts[0],
 			notes:   parts[1],
 			status:  status,
-		}
-
-		tc.addTask(t)
+		})
 	}
 }
 
+func saveData(l *List) {
+	return
+}
+
 func main() {
+	// underlying array
 	task_array := [capacity]Task{}
 	// create a slice of array
-	task_list := TaskContainer{slc: task_array[:0]}
+	task_list := List{s: task_array[:0]}
 	loadData(&task_list)
+
+	// Main Loop
+	var flag bool = true
+	for flag {
+
+		// Show Task list
+		task_list.printTasks()
+
+		fmt.Println()
+		fmt.Print("(1) Add Task; (2) Mark as Done; (3) Remove Task; (0) Exit")
+		fmt.Print(": ")
+
+		var selection int
+		_, err := fmt.Scanf("%d", &selection)
+
+		// if wrong input is given, ignore it because idk how to catch errors in golang
+		if err != nil {
+			continue
+		}
+
+		switch selection {
+
+		case 0:
+			fmt.Println("Exiting...")
+		case 1:
+			fmt.Println("Adding task...")
+		case 2:
+			task_list.printTasks()
+			fmt.Print(": ")
+			_, err := fmt.Scanf("%d", &selection)
+			if err != nil {
+				continue
+			}
+			task_list.markAsDone(selection - 1)
+
+		case 3:
+			fmt.Println("removing task...")
+		default:
+			fmt.Println("please enter a number from 1-4 or use ctrl-c")
+
+		}
+
+	}
 
 }
